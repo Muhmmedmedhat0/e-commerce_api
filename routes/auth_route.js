940ad5840fbf3b9745c83e62c318adea5165fda2
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-07-17 21:47:16 +0200

    add auth

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -4,4 +4,7 @@ const authCtrl = require('../controllers/auth_ctrl');
 // /rigister =>POST
 router.post('/register', authCtrl.register);
 
+// /login =>POST
+router.post('/login', authCtrl.login);
+
 module.exports = router;
