80ca026c5401a07ef2f587c7087f395aa7acc3f5
Muhmmed Medhat <muhmmedmedhat0@gmail.com>
2022-08-26 08:54:07 +0200

    fixing bugs

--- routes/product.js
+++ routes/product.js
@@ -2,19 +2,14 @@ const router = require('express').Router();
 const productController = require('../controllers/product');
 const { verifyTokenAndAdmin } = require('../middleware/verifyToken');
 
-// /api/products => POST
 router.post('/', verifyTokenAndAdmin, productController.create);
 
-// /api/products/:id => =>PUT
 router.put('/:id', verifyTokenAndAdmin, productController.productUpdate);
 
-// /api/products/:id => =>DELETE
 router.delete('/:id', verifyTokenAndAdmin, productController.ProductDelete);
 
-// /api/find/:id => GET
 router.get('/find/:id', productController.getProdctt);
 
-// /api/products => GET
 router.get('/', productController.productGetAll);
 
 module.exports = router;
